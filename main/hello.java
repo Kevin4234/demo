754d19bcb74ac506ded2fdb42de55cc68c8177b5
zhouwk <zhouwk@2345.com>
2018-07-24 17:49:04 +0800

    第二次提交

--- main/hello.java
+++ main/hello.java
@@ -1,8 +1,10 @@
+package main;
+
 /**
  * Created by zhouwk on 2018/7/24.
  */
 public class hello {
     public static void main(String args[]){
-        System.out.println("hello  world");
+        System.out.println("我变了");
     }
 }
